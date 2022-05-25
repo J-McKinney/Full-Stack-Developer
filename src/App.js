fcd175b113bb9eb2c58ccd6e4e54e3f45a5dcbed
Jesse <jessemckinney513@gmail.com>
2022-05-25 15:23:29 -0400

    yay

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ function App() {
   return (
     <>
       <div className="App">
-      <Landing />
-    </div>
+        <Landing />
+      </div>
     </>
   );
 }
--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -1,57 +1,10 @@
-import React, { useEffect } from "react";
-import anime from "animejs";
-import logo from "../../logo.svg";
+import React from "react";
 import Style from "./Landing.module.css";
-import "./animatedBG.css";
 
 function Landing() {
-  useEffect(() => {
-    const animateBlocks = () => {
-      let container = document.querySelector(".BGContainer");
-      for (var i = 0; i <= 60; i++) {
-        let blocks = document.createElement("div");
-        blocks.classList.add("block");
-        container.appendChild(blocks);
-      }
-
-      anime({
-        targets: ".block",
-        translateX: function () {
-          return anime.random(-700, 700);
-        },
-        translateY: function () {
-          return anime.random(-250, 400);
-        },
-        rotate: function () {
-          return anime.random(0, 350);
-        },
-        scale: function () {
-          return anime.random(10, 50);
-        },
-        // loop: true,
-        direction: "alternate",
-        easing: "linear",
-        duration: 1000,
-        delay: anime.stagger(200),
-      });
-    };
-    animateBlocks();
-  }, []);
-
   return (
     <>
-      <div id={Style.BGWrapper} className="BGWrapper">
-        <div id={Style.BGContainer} className="BGContainer">
-          <div className={Style.App}>
-            <header className={Style.AppHeader}>
-              <img src={logo} className={Style.AppLogo} alt="logo" />
-              <h1 className={Style.code}>
-              Jesse McKinney - Full Stack Developer
-              </h1>
-            </header>
-          </div>
-        </div>
-      </div>
+      <div className={Style.App}>Landing</div>
     </>
   );
 }
