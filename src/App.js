056a104ac5cd2ce5da65db86bc4679493107c072
Jesse <jessemckinney513@gmail.com>
2022-05-25 16:43:29 -0400

    working routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Landing from "./pages/Landing/Landing";
+import Main from "./pages/Main/Main";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <div className="App">
-        <Landing />
-      </div>
+      <Router>
+        <Routes>
+          <Route path="/Full-Stack-Developer" element={<Landing />} />
+          <Route path="/Main" element={<Main />} />
+        </Routes>
+      </Router>
     </>
   );
 }
--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -1,10 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Tech from "../../img/tech.jpg";
 import Style from "./Landing.module.css";
 
-function Landing() {
+// function Landing() {
+  const Landing = () => {
   return (
     <>
-      <div className={Style.App}>Landing</div>
+      <div className={Style.App}>
+        <div className={Style.AppHeader}>
+          <img className={Style.techImg} src={Tech} alt="Alt" />
+          <Link className={Style.button} to="/Main">
+            <button className={Style.button}>Enter</button>
+          </Link>
+        </div>
+      </div>
     </>
   );
 }
