7ccc1bd1999c0a0a8f269f47fb8258f6eb360e70
Jesse McKinney <jessemckinney513@gmail.com>
2025-12-02 16:43:45 -0500

    Fixing Routing

--- README.md
+++ README.md
@@ -5,3 +5,6 @@
 ## Full Stack Developer
 
 ### Pages are modified in one location: `src/content_option.js`.
+
+
+AbortedDeferredError, Await, BrowserRouter, Form, HashRouter, Link, MemoryRouter, NavLink, Navigate, NavigationType, Outlet, Route, Router, RouterProvider, Routes, ScrollRestoration, UNSAFE_DataRouterContext, UNSAFE_DataRouterStateContext, UNSAFE_ErrorResponseImpl, UNSAFE_FetchersContext, UNSAFE_LocationContext, UNSAFE_NavigationContext, UNSAFE_RouteContext, UNSAFE_ViewTransitionContext, UNSAFE_useRouteId, UNSAFE_useScrollRestoration, createBrowserRouter, createHashRouter, createMemoryRouter, createPath, createRoutesFromChildren, createRoutesFromElements, createSearchParams, defer, generatePath, isRouteErrorResponse, json, matchPath, matchRoutes, parsePath, redirect, redirectDocument, renderMatches, replace, resolvePath, unstable_HistoryRouter, unstable_usePrompt, useActionData, useAsyncError, useAsyncValue, useBeforeUnload, useBlocker, useFetcher, useFetchers, useFormAction, useHref, useInRouterContext, useLinkClickHandler, useLoaderData, useLocation, useMatch, useMatches, useNavigate, useNavigation, useNavigationType, useOutlet, useOutletContext, useParams, useResolvedPath, useRevalidator, useRouteError, useRouteLoaderData, useRoutes, useSearchParams, useSubmit, useViewTransitionState
--- src/app/App.js
+++ src/app/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, useLocation } from "react-router-dom";
+// import { HashRouter as Router, useLocation } from "react-router-dom";
 import withRouter from "../hooks/withRouter";
 import AppRoutes from "./routes";
 import Headermain from "../header";
@@ -17,7 +18,8 @@ const ScrollToTop = withRouter( _ScrollToTop );
 
 export default function App() {
     return (
-        <Router basename={process.env.PUBLIC_URL}>
+        // <Router basename={process.env.PUBLIC_URL}>
+        <Router>
             <ScrollToTop>
                 <Headermain />
                 <AppRoutes />
@@ -25,3 +27,65 @@ export default function App() {
         </Router>
     );
 }
+
+
+
+
+
+// import React, { useEffect } from "react";
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import { BrowserRouter as Router, useLocation, Route, Routes } from "react-router-dom";
+// // import { HashRouter as Router, useLocation, Route, Routes  } from "react-router-dom";
+// import withRouter from "../hooks/withRouter";
+
+// import Headermain from "../header";
+
+// import { Home } from "../pages/home";
+// import { Portfolio } from "../pages/portfolio";
+// import { ContactUs } from "../pages/contact";
+// import { About } from "../pages/about";
+// import { CSSTransition, TransitionGroup } from "react-transition-group";
+// import "./App.css";
+
+// const AnimatedRoutes = withRouter(({ location }) => (
+//     <TransitionGroup>
+//         <CSSTransition
+//             key={location.key}
+//             timeout={{
+//                 enter: 400,
+//                 exit: 400,
+//             }}
+//             classNames="page"
+//             unmountOnExit
+//         >
+//             <Routes location={location}>
+//                 <Route exact path="/" element={<Home />} />
+//                 <Route path="/about" element={<About />} />
+//                 <Route path="/portfolio" element={<Portfolio />} />
+//                 <Route path="/contact" element={<ContactUs />} />
+//                 <Route path="*" element={<Home />} />
+//             </Routes>
+//         </CSSTransition>
+//     </TransitionGroup>
+// ));
+
+// function _ScrollToTop(props) {
+//     const { pathname } = useLocation();
+//     useEffect(() => {
+//         window.scrollTo(0, 0);
+//     }, [pathname]);
+//     return props.children;
+// }
+// const ScrollToTop = withRouter( _ScrollToTop );
+
+// export default function App() {
+//     return (
+//         // <Router basename={process.env.PUBLIC_URL}>
+//         <Router>
+//             <ScrollToTop>
+//                 <Headermain />
+//                 <AnimatedRoutes />
+//             </ScrollToTop>
+//         </Router>
+//     );
+// }
