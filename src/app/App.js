b9dc338414093a2fc961090040d66432cc54ce91
Jesse McKinney <jessemckinney513@gmail.com>
2025-12-03 12:45:34 -0500

    Fixing Routing

--- src/app/App.js
+++ src/app/App.js
@@ -1,40 +1,6 @@
-// import React, { useEffect } from "react";
-// import "bootstrap/dist/css/bootstrap.min.css";
-// import { BrowserRouter as Router, useLocation } from "react-router-dom";
-// // import { HashRouter as Router, useLocation } from "react-router-dom";
-// import withRouter from "../hooks/withRouter";
-// import AppRoutes from "./routes";
-// import Headermain from "../header";
-// import "./App.css";
-
-// function _ScrollToTop(props) {
-//     const { pathname } = useLocation();
-//     useEffect(() => {
-//         window.scrollTo(0, 0);
-//     }, [pathname]);
-//     return props.children;
-// }
-// const ScrollToTop = withRouter( _ScrollToTop );
-
-// export default function App() {
-//     return (
-//         // <Router basename={process.env.PUBLIC_URL}>
-//         <Router>
-//             <ScrollToTop>
-//                 <Headermain />
-//                 <AppRoutes />
-//             </ScrollToTop>
-//         </Router>
-//     );
-// }
-
-
-
-
-
 import React, { useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, useLocation, Route, Routes, redirect } from "react-router-dom";
+import { BrowserRouter as Router, useLocation, Route, Routes } from "react-router-dom";
 import withRouter from "../hooks/withRouter";
 import Headermain from "../header";
 import { Home } from "../pages/home";
@@ -73,12 +39,12 @@ function _ScrollToTop(props) {
     }, [pathname]);
     return props.children;
 }
+
 const ScrollToTop = withRouter( _ScrollToTop );
 
 export default function App() {
     return (
         <Router basename={process.env.PUBLIC_URL}>
-        {/* <Router> */}
             <ScrollToTop>
                 <Headermain />
                 <AnimatedRoutes />
